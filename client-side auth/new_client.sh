4330bf20f81949bbba3676c2b7545ec92922f86d
ngdude <ngdude@mail.ru>
2020-08-10 17:00:13 +0300

    Update new_client.sh

--- client-side auth/new_client.sh
+++ client-side auth/new_client.sh
@@ -21,18 +21,12 @@ USERS_DIR="${SSL_CERTS_DIR}/users"
 
 mkdir -p ${USERS_DIR}
 
-# Create the Client Key and CSR
 openssl genrsa -out ${USERS_DIR}/${USERNAME}.key 1024
 openssl req -config $SSL_DIR/$1/openssl.cnf -nodes -new -key ${USERS_DIR}/${USERNAME}.key -out ${USERS_DIR}/${USERNAME}.csr -subj "/C=RU/ST=SSL/L=SSL/O=SSL/OU=dev/CN=ca/emailAddress=admin@mydomain.com"
 
-# Sign the client certificate with our CA cert.  Unlike signing our own server cert, this is what we want to do.
 openssl x509 -req -days 1095 -in ${USERS_DIR}/${USERNAME}.csr -CA $SSL_CERTS_DIR/ca.crt -CAkey $SSL_PRIVATE_DIR/ca.key -CAserial $SSL_DIR/${CA_NAME}/serial -CAcreateserial -out ${USERS_DIR}/${USERNAME}.crt
 
-#echo "making p12 file"
-#browsers need P12s (contain key and cert)
 openssl pkcs12 -export -clcerts -in ${USERS_DIR}/${USERNAME}.crt -inkey ${USERS_DIR}/${USERNAME}.key -out ${USERS_DIR}/${USERNAME}.p12 -passout pass:Qwerty@123
-
-#echo "made ${USERS_DIR}/${USERNAME}.p12"
 
 echo "Client-side certificate " | mutt -e "set crypt_use_gpgme=no" -a ${USERS_DIR}/${USERNAME}.p12  -s "new cert" -- $MAIL_TO
 
